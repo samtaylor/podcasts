592d93452e99556cce328df2c29e4b0d4bee2c7f
Samuel Taylor <Samuel.Taylor@bbc.co.uk>
2017-06-13 09:11:32 +0100

    added play/pause button to episode activity

--- mobile/src/main/java/samtaylor/podcasts/episode/EpisodeActivity.kt
+++ mobile/src/main/java/samtaylor/podcasts/episode/EpisodeActivity.kt
@@ -4,11 +4,15 @@ import android.arch.lifecycle.LifecycleActivity
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import samtaylor.podcasts.R
 
 class EpisodeActivity : LifecycleActivity()
 {
+    private var playing = false
+
     override fun onCreate( savedInstanceState: Bundle? )
     {
         super.onCreate( savedInstanceState )
@@ -22,6 +26,50 @@ class EpisodeActivity : LifecycleActivity()
             val episodeName = findViewById( R.id.episode_name ) as TextView
             episodeName.text = it?.title
         } )
+
+        val playPauseButton = findViewById( R.id.play_pause_button ) as Button
+        playPauseButton.setOnClickListener {
+            if ( playPauseButton.text == getString( R.string.play ) )
+            {
+                playPauseButton.text = getString( R.string.pause )
+            }
+            else
+            {
+                playPauseButton.text = getString( R.string.play )
+            }
+        }
+    }
+
+    fun playPauseButtonClicked( button: View )
+    {
+        if ( button is Button )
+        {
+            togglePlayback( button )
+        }
+    }
+
+    private fun togglePlayback( button: Button )
+    {
+        if ( button.text == getString( R.string.play ) )
+        {
+            button.text = getString( R.string.pause )
+            play()
+        }
+        else
+        {
+            button.text = getString( R.string.play )
+            pause()
+        }
+    }
+
+    private fun play()
+    {
+
+    }
+
+    private fun pause()
+    {
+
     }
 
     companion object
