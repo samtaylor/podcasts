f722f473c815df1de8877ef72fdb23fa332d2e42
Samuel Taylor <Samuel.Taylor@bbc.co.uk>
2017-06-13 14:22:11 +0100

    basic playback implemented

--- mobile/src/main/java/samtaylor/podcasts/episode/EpisodeActivity.kt
+++ mobile/src/main/java/samtaylor/podcasts/episode/EpisodeActivity.kt
@@ -3,15 +3,41 @@ package samtaylor.podcasts.episode
 import android.arch.lifecycle.LifecycleActivity
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import samtaylor.podcasts.R
+import samtaylor.podcasts.playback.PlaybackService
 
 class EpisodeActivity : LifecycleActivity()
 {
-    private var playing = false
+    private var playbackService: PlaybackService? = null
+
+    private var serviceBound = false
+
+    private val serviceConnection = PlaybackServiceConnection()
+
+    inner class PlaybackServiceConnection: ServiceConnection
+    {
+        override fun onServiceConnected( name: ComponentName?, service: IBinder? )
+        {
+            val binder = service as PlaybackService.LocalBinder
+            this@EpisodeActivity.playbackService = binder.getService()
+
+            this@EpisodeActivity.serviceBound = true
+        }
+
+        override fun onServiceDisconnected( name: ComponentName? )
+        {
+            this@EpisodeActivity.serviceBound = false
+        }
+    }
 
     override fun onCreate( savedInstanceState: Bundle? )
     {
@@ -26,18 +52,6 @@ class EpisodeActivity : LifecycleActivity()
             val episodeName = findViewById( R.id.episode_name ) as TextView
             episodeName.text = it?.title
         } )
-
-        val playPauseButton = findViewById( R.id.play_pause_button ) as Button
-        playPauseButton.setOnClickListener {
-            if ( playPauseButton.text == getString( R.string.play ) )
-            {
-                playPauseButton.text = getString( R.string.pause )
-            }
-            else
-            {
-                playPauseButton.text = getString( R.string.play )
-            }
-        }
     }
 
     fun playPauseButtonClicked( button: View )
@@ -50,21 +64,20 @@ class EpisodeActivity : LifecycleActivity()
 
     private fun togglePlayback( button: Button )
     {
-        if ( button.text == getString( R.string.play ) )
-        {
-            button.text = getString( R.string.pause )
-            play()
-        }
-        else
-        {
-            button.text = getString( R.string.play )
-            pause()
-        }
+        this.play()
     }
 
     private fun play()
     {
+        if ( !this.serviceBound )
+        {
+            val episodeId = this.intent.extras[ EXTRA_EPISODE_ID ] as Int
 
+            val playIntent = Intent( this, PlaybackService::class.java )
+            playIntent.putExtra( "media", "https://api.spreaker.com/v2/episodes/$episodeId/play" )
+            this.startService( playIntent )
+            this.bindService( playIntent, this.serviceConnection, Context.BIND_AUTO_CREATE )
+        }
     }
 
     private fun pause()
--- mobile/src/main/java/samtaylor/podcasts/playback/PlaybackService.kt
+++ mobile/src/main/java/samtaylor/podcasts/playback/PlaybackService.kt
@@ -1,11 +1,13 @@
 package samtaylor.podcasts.playback
 
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.media.AudioManager
 import android.media.MediaPlayer
 import android.os.Binder
 import android.os.IBinder
+import android.util.Log
 
 class PlaybackService: Service(),
                        MediaPlayer.OnCompletionListener,
@@ -16,39 +18,218 @@ class PlaybackService: Service(),
                        MediaPlayer.OnBufferingUpdateListener,
                        AudioManager.OnAudioFocusChangeListener
 {
-    override fun onAudioFocusChange(focusChange: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    private var mediaPlayer: MediaPlayer? = null
+
+    private var mediaFile: String? = null
+
+    private var resumePosition = 0
+
+    private var audioManager: AudioManager? = null
+
+    override fun onStartCommand( intent: Intent?, flags: Int, startId: Int ): Int
+    {
+        this.mediaFile = intent?.extras?.get( "media" ) as String
+
+        if ( this.mediaFile != null && !this.mediaFile.equals( "" ) )
+        {
+            if ( !this.requestAudioFocus() )
+            {
+                this.stopSelf()
+            }
+
+            this.initMediaPlayer()
+        }
+        else
+        {
+            this.stopSelf()
+        }
+
+        return super.onStartCommand( intent, flags, startId )
     }
 
-    override fun onCompletion(mp: MediaPlayer?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onDestroy()
+    {
+        super.onDestroy()
+
+        this.mediaPlayer?.let {
+            this.stopMedia()
+            it.release()
+        }
+
+        this.removeAudioFocus()
     }
 
-    override fun onPrepared(mp: MediaPlayer?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    private fun initMediaPlayer()
+    {
+        this.mediaPlayer = MediaPlayer()
+
+        this.mediaPlayer?.let {
+            it.setOnCompletionListener( this )
+            it.setOnPreparedListener( this )
+            it.setOnErrorListener( this )
+            it.setOnSeekCompleteListener ( this )
+            it.setOnInfoListener( this )
+            it.setOnBufferingUpdateListener( this )
+
+            it.reset()
+
+            it.setAudioStreamType( AudioManager.STREAM_MUSIC )
+
+            it.setDataSource( this.mediaFile )
+
+            it.prepareAsync()
+        }
     }
 
-    override fun onError(mp: MediaPlayer?, what: Int, extra: Int): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    private fun playMedia()
+    {
+        this.mediaPlayer?.let {
+            if ( !it.isPlaying )
+            {
+                it.start()
+            }
+        }
+    }
+
+    private fun stopMedia()
+    {
+        this.mediaPlayer?.let {
+            if ( it.isPlaying )
+            {
+                it.stop()
+            }
+        }
+    }
+
+    private fun pauseMedia()
+    {
+        this.mediaPlayer?.let {
+            if ( it.isPlaying )
+            {
+                it.pause()
+                this.resumePosition = it.currentPosition
+            }
+        }
+    }
+
+    private fun resumeMedia()
+    {
+        this.mediaPlayer?.let {
+            if ( !it.isPlaying )
+            {
+                it.seekTo( this.resumePosition )
+                it.start()
+            }
+        }
+    }
+
+    private fun requestAudioFocus(): Boolean
+    {
+        this.audioManager = this.getSystemService( Context.AUDIO_SERVICE ) as AudioManager
+        val result = this.audioManager?.requestAudioFocus( this, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN )
+
+        return result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED
+    }
+
+    private fun removeAudioFocus(): Boolean
+    {
+        return AudioManager.AUDIOFOCUS_REQUEST_GRANTED == this.audioManager?.abandonAudioFocus( this )
+    }
+
+    override fun onAudioFocusChange( focusChange: Int )
+    {
+        when( focusChange )
+        {
+            AudioManager.AUDIOFOCUS_GAIN -> {
+                if ( this.mediaPlayer == null )
+                {
+                    this.initMediaPlayer()
+                }
+
+                this.mediaPlayer?.let {
+                    if ( !it.isPlaying )
+                    {
+                        it.start()
+                    }
+                    it.setVolume( 1.0F, 1.0F )
+                }
+            }
+
+            AudioManager.AUDIOFOCUS_LOSS -> {
+                this.mediaPlayer?.let {
+                    if ( it.isPlaying )
+                    {
+                        it.stop()
+                        it.release()
+                        this.mediaPlayer = null
+                    }
+                }
+            }
+
+            AudioManager.AUDIOFOCUS_LOSS_TRANSIENT -> {
+                this.mediaPlayer?.let {
+                    if ( it.isPlaying ) it.pause()
+                }
+            }
+
+            AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK -> {
+                this.mediaPlayer?.let {
+                    if ( it.isPlaying ) it.setVolume( 0.1F, 0.1F )
+                }
+            }
+        }
+    }
+
+    override fun onCompletion( mediaPlayer: MediaPlayer? )
+    {
+        this.stopMedia()
+        this.stopSelf()
+    }
+
+    override fun onPrepared( mp: MediaPlayer? )
+    {
+        this.playMedia()
+    }
+
+    override fun onError( mediaPlayer: MediaPlayer?, what: Int, extra: Int ): Boolean
+    {
+        when( what )
+        {
+            MediaPlayer.MEDIA_ERROR_NOT_VALID_FOR_PROGRESSIVE_PLAYBACK -> {
+                Log.d( "PlaybackService", "Not valid for progressive playback - $extra" )
+            }
+
+            MediaPlayer.MEDIA_ERROR_SERVER_DIED ->
+            {
+                Log.d( "PlaybackService", "Server died - $extra" )
+            }
+
+            MediaPlayer.MEDIA_ERROR_UNKNOWN ->
+            {
+                Log.d( "PlaybackService", "Unknown - $extra" )
+            }
+        }
+
+        return false
     }
 
     override fun onSeekComplete(mp: MediaPlayer?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
     override fun onInfo(mp: MediaPlayer?, what: Int, extra: Int): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return true
     }
 
     override fun onBufferingUpdate(mp: MediaPlayer?, percent: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
     private val binder: IBinder = LocalBinder()
 
     override fun onBind( intent: Intent? ): IBinder
     {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return this.binder
     }
 
     inner class LocalBinder: Binder()
